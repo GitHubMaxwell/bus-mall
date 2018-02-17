8ede3f66bd302abc96f09894910bf47d1de51238
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-02-17 15:37:34 -0800

    deleted comments

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 'use strict';
 
 var allImages = [];
-var numOfClicks = 0; //same as totalClicks, you should incriment this in your handler function +=
-var justViewed = []; //how am i pushing data to this?
+var numOfClicks = 0;
+var justViewed = [];
 var tally = document.getElementById('tally');
 
 var imgElOne = document.getElementById('imgOne');
 var imgElTwo = document.getElementById('imgTwo');
 var imgElThree = document.getElementById('imgThree');
 
-//constructor function
 function Image(name, filepath) {
   this.name = name;
   this.filepath = filepath;
@@ -36,12 +35,10 @@ new Image('usb', 'img/usb.gif');
 new Image('water-can', 'img/water-can.jpg');
 new Image('wine-glass', 'img/wine-glass.jpg');
 
-//randomly generate a number
 function randNum() {
   return Math.floor(Math.random() * allImages.length);
 }
 
-//this generates and checks images arent the same
 function randChecker () {
   var currentShow = [];
   currentShow[0] = randNum();
@@ -60,15 +57,12 @@ function randChecker () {
     console.log('check index three: ' + currentShow[2]);
   }
   console.log(currentShow);
-  //display the images
   imgElOne.src = allImages[currentShow[0]].filepath;
   imgElTwo.src = allImages[currentShow[1]].filepath;
   imgElThree.src = allImages[currentShow[2]].filepath;
-
-  //add to view
   for (var i = 0; i < 3; i++) {
     allImages[currentShow[i]].views += 1;
-    justViewed[i] = currentShow[i]; //storing the justViewed
+    justViewed[i] = currentShow[i];
     console.log('just viewed: ' + justViewed[i]);
   }
 }
@@ -91,7 +85,6 @@ function handleClick() {
     numOfClicks += 1;
     allImages[justViewed[0]].votes += 1;
   }
-  //imgElOne.addEventListener('click', handleClick);
   randChecker();
 }
 
@@ -125,17 +118,4 @@ function removeListener(){
   imgElTwo.removeEventListener('click',handleClick2);
   imgElThree.removeEventListener('click',handleClick3);
 }
-//handleClick();
 randChecker();
-
-//indi function disable event listener
-//indi function handles action on click either or per img
-
-/*
-  for (var i = 0; i < allImages.length; i++) {
-    if (event.target.id === allImages[i].name) {
-      allImages[i].votes += 1;
-      console.log(event.target.id + ' has ' + allImages[i].votes + ' votes in ' + allImages[i].views + ' views ');
-    }
-  }
-  */
