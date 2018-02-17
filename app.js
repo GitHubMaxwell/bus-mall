10effced52f5d70e07909547a6946799a3718761
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-02-17 15:30:02 -0800

    lab 11 complete

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 'use strict';
 
-Image.allImages = [];
-var indexArray = []; //master array that the constructor function pushes the new instances to
+var allImages = [];
 var numOfClicks = 0; //same as totalClicks, you should incriment this in your handler function +=
 var justViewed = []; //how am i pushing data to this?
-Image.tally = document.getElementById('tally');
+var tally = document.getElementById('tally');
+
+var imgElOne = document.getElementById('imgOne');
+var imgElTwo = document.getElementById('imgTwo');
+var imgElThree = document.getElementById('imgThree');
 
 //constructor function
 function Image(name, filepath) {
@@ -13,7 +16,7 @@ function Image(name, filepath) {
   this.filepath = filepath;
   this.votes = 0;
   this.views = 0;
-  Image.allImages.push(this);
+  allImages.push(this);
 }
 
 new Image('bag','img/bag.jpg');
@@ -33,30 +36,16 @@ new Image('usb', 'img/usb.gif');
 new Image('water-can', 'img/water-can.jpg');
 new Image('wine-glass', 'img/wine-glass.jpg');
 
-
-
-
 //randomly generate a number
 function randNum() {
-  return Math.floor(Math.random() * Image.length);
+  return Math.floor(Math.random() * allImages.length);
 }
 
-//randomly display one of the pictures. need to run this three times and have IF statements making sure that there arent doubles and that they dont match the previous three set
-/*function randomPicture() { //this should be closer to the displayPictureFunction
-  var randomIndexOne = Math.floor(Math.random() * Image.allImages.length);
-  var randomIndexTwo = Math.floor(Math.random() * Image.allImages.length);
-  var randomIndexThree = Math.floor(Math.random() * Image.allImages.length);
-  indexArray.push(randomIndexOne);
-  indexArray.push(randomIndexTwo);
-  indexArray.push(randomIndexThree);
-  randChecker();//i need to put this so it doesnt fire until the randChecker??
-}*/
-
-//needs to be a function that check if the number are equal to eachother before allowign them to be pushed to eht page (stored in the array)
+//this generates and checks images arent the same
 function randChecker () {
   var currentShow = [];
   currentShow[0] = randNum();
-  while (justViewed.indexOf(currentShow[0])) {
+  while (justViewed.indexOf(currentShow[0]) !== -1) {
     currentShow[0] = randNum();
     console.log('check index one: ' + currentShow[0]);
   }
@@ -70,51 +59,83 @@ function randChecker () {
     currentShow[2] = randNum();
     console.log('check index three: ' + currentShow[2]);
   }
-  imgElOne.src = Image.allImages[indexArray[0]].filepath;
-  imgElTwo.src = Image.allImages[indexArray[1]].filepath;
-  imgElThree.src = Image.allImages[indexArray[2]].filepath;
+  console.log(currentShow);
+  //display the images
+  imgElOne.src = allImages[currentShow[0]].filepath;
+  imgElTwo.src = allImages[currentShow[1]].filepath;
+  imgElThree.src = allImages[currentShow[2]].filepath;
 
+  //add to view
   for (var i = 0; i < 3; i++) {
-    /*.pics[i].src = indexArray[currentShow[i]];
-    Product.pics[i].id = indexArray[currentShow[i]].name;*/
-    indexArray[currentShow[i]].views += 1;
+    allImages[currentShow[i]].views += 1;
     justViewed[i] = currentShow[i]; //storing the justViewed
+    console.log('just viewed: ' + justViewed[i]);
   }
 }
 
-var imgElOne = document.getElementById('imgOne');
-var imgElTwo = document.getElementById('imgTwo');
-var imgElThree = document.getElementById('imgThree');
+function showTally() {
+  for(var i = 0; i < allImages.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = allImages[i].name + ' has ' + allImages[i].votes + ' votes in ' + allImages[i].views + ' views ';
+    tally.appendChild(liEl);
+  }
+}
 
-function handleClick(event) {
-  console.log(numOfClicks, 'total clicks');
-  if (numOfClicks > 24) {
-    imgElOne.removeEventListener('click',handleClick);
-    imgElTwo.removeEventListener('click',handleClick);
-    imgElThree.removeEventListener('click',handleClick);
+imgElOne.addEventListener('click', handleClick);
+function handleClick() {
+  console.log(numOfClicks, ' total clicks');
+  if (numOfClicks === 25) {
+    removeListener();
+    showTally();
+  } else {
+    numOfClicks += 1;
+    allImages[justViewed[0]].votes += 1;
   }
-  if (numOfClicks > 24) {
-    imgElOne.removeEventListener('click',handleClick);
-    imgElTwo.removeEventListener('click',handleClick);
-    imgElThree.removeEventListener('click',handleClick);
-  }
-  if (numOfClicks > 24) {
-    imgElOne.removeEventListener('click',handleClick);
-    imgElTwo.removeEventListener('click',handleClick);
-    imgElThree.removeEventListener('click',handleClick);
+  //imgElOne.addEventListener('click', handleClick);
+  randChecker();
+}
+
+
+imgElTwo.addEventListener('click', handleClick2);
+function handleClick2() {
+  if (numOfClicks === 25) {
+    removeListener();
+    showTally();
+  } else {
+    numOfClicks += 1;
+    allImages[justViewed[1]].votes += 1;
   }
   randChecker();
-  function showTally() {
-    for(var i = 0; i < indexArray.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = indexArray[i].name + ' has ' + indexArray[i].votes + ' votes in ' + indexArray[i].views + ' views ';
-      Image.tally.appendChild(liEl);
+}
+
+imgElThree.addEventListener('click', handleClick3);
+function handleClick3() {
+  if (numOfClicks === 25) {
+    removeListener();
+    showTally();
+  } else {
+    numOfClicks += 1;
+    allImages[justViewed[2]].votes += 1;
+  }
+  randChecker();
+}
+
+function removeListener(){
+  imgElOne.removeEventListener('click',handleClick);
+  imgElTwo.removeEventListener('click',handleClick2);
+  imgElThree.removeEventListener('click',handleClick3);
+}
+//handleClick();
+randChecker();
+
+//indi function disable event listener
+//indi function handles action on click either or per img
+
+/*
+  for (var i = 0; i < allImages.length; i++) {
+    if (event.target.id === allImages[i].name) {
+      allImages[i].votes += 1;
+      console.log(event.target.id + ' has ' + allImages[i].votes + ' votes in ' + allImages[i].views + ' views ');
     }
   }
-
-  imgElOne.addEventListener('click', handleClick);
-  imgElTwo.addEventListener('click', handleClick);
-  imgElThree.addEventListener('click', handleClick);
-  randChecker();
-}
-randChecker();
+  */
